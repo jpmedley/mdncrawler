140ac0368c371af1be3669b76603fd0c11c89b5d
Glitch (sleet-dagger) <none>
2018-03-05 20:50:57 +0000

    🌉💼 Checkpoint README.md:111231/22 public/index.html:111231/26 server.js:111231/340 public/cars.html:111231/15

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express'),
     app = express(),
     puppeteer = require('puppeteer');
 
+app.use(express.static('public'));
+
 app.get("/", async (request, response) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://developers.google.com/web/tools/puppeteer');
-    await page.screenshot({path: 'puppeteer.png'});
-    await browser.close();
-    response.sendFile('/app/puppeteer.png');
+    await page.goto('https://sleet-dagger.glitch.me/cars.html');
+    await page.waitForSelector('select');
+    response.send(document.querySelector('select'));
   } catch (error) {
     response.status(503).end(error.message);
   }
