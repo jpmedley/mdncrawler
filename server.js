ef5f7e66017324afbf3821487597dd637fe00695
Glitch (mdn-scraper) <none>
2018-03-16 12:02:57 +0000

    🐟⚾️ Checkpoint ./server.js:111231/561 ./static/index.html:111231/228

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const express = require('express'),
     app = express(),
     puppeteer = require('puppeteer');
 
-app.use(express.static('public'));
+app.use(express.static('static'));
 
-app.get("/", async (request, response) => {
+app.get("/:interface", async (request, response) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://puppeteer-queryselector-eval-example.glitch.me/cars.html');
-    const selector = 'select';
-    const name = await page.$eval(selector, node => node.name);
-    response.send(name);
+    await page.goto(`https://developer.mozilla.org/en-US/docs/Web/API/${request.params.interface}`);
+    const table = page.$('#compat-desktop');
+    response.type('image/png').send(await page.screenshot({fullPage: true}));
     await browser.close();
   } catch (error) {
     response.status(503).end(error.message);
