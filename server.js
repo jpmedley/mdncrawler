100c63c1570c8f80d47e8ed6a9f7380781525524
Glitch (mdn-scraper) <none>
2018-03-16 12:13:32 +0000

    🎏👊 Checkpoint ./server.js:111231/689

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ app.get("/:interface", async (request, response) => {
     });
     const page = await browser.newPage();
     await page.goto(`https://developer.mozilla.org/en-US/docs/Web/API/${request.params.interface}`);
-    const table = page.$('#compat-desktop');
-    response.type('image/png').send(await page.screenshot({fullPage: true}));
+    const headers = await page.$$('#compat-desktop th');
+    const browsers = [];
+    for (let i = 0; i < headers.length; i++) {
+      const name = await headers[i].getProperty('textContent');
+      browsers.push(name);
+    }
+    const string = browsers.concat(' ');
+    response.send(string);
     await browser.close();
   } catch (error) {
     response.status(503).end(error.message);
