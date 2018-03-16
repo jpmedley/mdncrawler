4b60df887a4ba2d051764bd6c45e927ce6b71fbe
Glitch (grave-limit) <none>
2018-03-16 14:03:30 +0000

    🍌🍠 Checkpoint ./server.js:111231/581

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ const browserIds = {
     ]
   },
   "edge_mobile": {
-
+    "aliases": [
+      
+    ]
   },
   "firefox": {
     "aliases": [
-      
+      "Firefox (Gecko)"
     ]
   },
   "firefox_android": {
@@ -33,39 +35,39 @@ const browserIds = {
   },
   "ie": {
     "aliases": [
-      
+      "Internet Explorer"
     ]
   },
   "opera": {
     "aliases": [
-      
+      "Opera"
     ]
   },
   "safari": {
     "aliases": [
-      
+      "Safari (WebKit)"
     ]
   }
-}
+};
 
 function getId(name) {
   for (let id in browserIds) {
-    console.log(browserIds[id].aliases);
+    
     if (browserIds[id].aliases.includes(name)) {
       return id;
-    } else {
-      return undefined;
     }
   }
+  return undefined;
 }
 
 async function getBrowsers(row) {
   const browsers = [];
   const cells = await row.$$('th');
   if (cells.length === 0) throw Error('The row that was passed to getBrowsers() has no <th> elements.');
-  for (let i = 1; i < cells.length; i++) {
+  for (let i = 0; i < cells.length; i++) {
     const content = await cells[i].getProperty('textContent');
     const value = await content.jsonValue();
+    if (value === 'Feature') continue;
     const id = getId(value);
     if (!id) throw Error(`Browser name ${value} is not defined in browserIds.`);
     browsers.push(id);
@@ -76,10 +78,10 @@ async function getBrowsers(row) {
 app.use(express.static('static'));
 
 app.get("/:interface", async (request, response) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']
+  });
   try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
     const page = await browser.newPage();
     await page.goto(`https://developer.mozilla.org/en-US/docs/Web/API/${request.params.interface}`);
     const rows = await page.$$('#compat-desktop tr');
@@ -89,6 +91,7 @@ app.get("/:interface", async (request, response) => {
     await browser.close();
   } catch (error) {
     response.status(503).end(error.message);
+    await browser.close();
   }
 });
 
